1a6ec8c788ab1a5a7c170bb535091c456a4daedc
Van Dinh Tam HUYNH <hvdtam@gmail.com>
2023-06-15 20:26:39 +0700

    add migration

---
+++ database/migrations/20230615_201530_shortlink.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/beego/beego/v2/client/orm/migration"
+)
+
+// DO NOT MODIFY
+type Shortlink_20230615_201530 struct {
+	migration.Migration
+}
+
+// DO NOT MODIFY
+func init() {
+	m := &Shortlink_20230615_201530{}
+	m.Created = "20230615_201530"
+
+	migration.Register("Shortlink_20230615_201530", m)
+}
+
+// Run the migrations
+func (m *Shortlink_20230615_201530) Up() {
+	// use m.SQL("CREATE TABLE ...") to make schema update
+	m.SQL("CREATE TABLE shortlink(id_shortlink serial primary key,long_url TEXT NOT NULL,alias_url TEXT NOT NULL,password TEXT NOT NULL,expire integer DEFAULT NULL,total_click integer DEFAULT NULL,created_at integer DEFAULT NULL,created_by integer DEFAULT NULL,updated_at integer DEFAULT NULL,updated_by integer DEFAULT NULL)")
+}
+
+// Reverse the migrations
+func (m *Shortlink_20230615_201530) Down() {
+	// use m.SQL("DROP TABLE ...") to reverse schema update
+	m.SQL("DROP TABLE shortlink")
+}
---
+++ database/migrations/20230615_202514_users.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/beego/beego/v2/client/orm/migration"
+)
+
+// DO NOT MODIFY
+type Users_20230615_202514 struct {
+	migration.Migration
+}
+
+// DO NOT MODIFY
+func init() {
+	m := &Users_20230615_202514{}
+	m.Created = "20230615_202514"
+
+	migration.Register("Users_20230615_202514", m)
+}
+
+// Run the migrations
+func (m *Users_20230615_202514) Up() {
+	// use m.SQL("CREATE TABLE ...") to make schema update
+	m.SQL("CREATE TABLE users(id_user serial primary key,username TEXT NOT NULL,password_hash TEXT NOT NULL,email TEXT NOT NULL,status integer DEFAULT NULL,last_online integer DEFAULT NULL,created_at integer DEFAULT NULL,updated_at integer DEFAULT NULL)")
+}
+
+// Reverse the migrations
+func (m *Users_20230615_202514) Down() {
+	// use m.SQL("DROP TABLE ...") to reverse schema update
+	m.SQL("DROP TABLE users")
+}
